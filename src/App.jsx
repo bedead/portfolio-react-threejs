04176cfeee9eb4e64b72a96c1a862ec62bc26b35
Satyam Mishra <satyam.work.only@gmail.com>
2023-04-22 17:17:56 +0530

    base site

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter } from "react-router-dom";
+import { Navbar, Hero, About, Works, Experience, Tech, Contact } from "./components";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <div className="relative z-10">
+          <Contact />
+        </div>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,22 @@
+import Hero from "./Hero";
+import Navbar from "./NavBar";
+import About from "./About";
+import Tech from "./Tech";
+import Experience from "./Experience";
+import Works from "./Works";
+import Contact from "./Contact";
+import CanvasLoader from "./Loader";
+import { BallCanvas } from "./canvas";
+
+
+export {
+    Hero,
+    Navbar,
+    About,
+    Tech,
+    Experience,
+    Works,
+    Contact,
+    CanvasLoader,
+    BallCanvas,
+};
