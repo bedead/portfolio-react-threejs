28120424263adcc02e969e9a81481106091e8211
Satyam Mishra <satyam.work.only@gmail.com>
2023-04-24 00:51:36 +0530

    added single skills showcase ball | canvas problem fixed

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -8,11 +8,6 @@ import texture from '../assets/texture.png'
 const Tech = () => {
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {/* {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))} */}
       <div className='xs:w-28 xs:h-28 sm:w-48 sm:h-48 md:w-72 md:h-72 lg:w-96 lg:h-96' key='skillsIcons'>
         <BallCanvas icon={texture} />
       </div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,23 +1,3 @@
-// import html from '../assets/tech/html.png'
-// import css from '../assets/tech/css.png'
-// import javascript from '../assets/tech/javascript.png'
-// import reactjs from '../assets/tech/reactjs.png'
-// import tailwind from '../assets/tech/tailwind.png'
-// import git from '../assets/tech/git.png'
-// import bootstrap from '../assets/tech/bootstrap.png'
-// import c_ from '../assets/tech/c++.png'
-// import django from '../assets/tech/django.png'
-// import figma from '../assets/tech/figma.png'
-// import firebase from '../assets/tech/firebase.png'
-// import flask from '../assets/tech/flask.png'
-// import inkscape from '../assets/tech/inkscape.png'
-// import java from '../assets/tech/java.png'
-// import mysql from '../assets/tech/mysql.png'
-// import photoshop from '../assets/tech/photoshop.png'
-// import python from '../assets/tech/python.png'
-// import github from '../assets/tech/github.png'
-
-
 import web from '../assets/web.png'
 import mobile from '../assets/mobile.png'
 import creator from '../assets/creator.png'
@@ -51,81 +31,6 @@ const services = [
         icon: mobile,
     },
 ];
-
-// const technologies = [
-//     {
-//         name: "HTML 5",
-//         icon: html,
-//     },
-//     {
-//         name: "CSS 3",
-//         icon: css,
-//     },
-//     {
-//         name: "JavaScript",
-//         icon: javascript,
-//     },
-//     {
-//         name: "React JS",
-//         icon: reactjs,
-//     },
-//     {
-//         name: "Tailwind CSS",
-//         icon: tailwind,
-//     },
-//     {
-//         name: "git",
-//         icon: git,
-//     },
-//     {
-//         name: "figma",
-//         icon: figma,
-//     },
-//     {
-//         name: "bootstrap",
-//         icon: bootstrap,
-//     },
-//     {
-//         name: "c++",
-//         icon: c_,
-//     },
-//     {
-//         name: "django",
-//         icon: django,
-//     },
-//     {
-//         name: "firebase",
-//         icon: firebase,
-//     },
-//     {
-//         name: "flask",
-//         icon: flask,
-//     },
-//     {
-//         name: "inkscape",
-//         icon: inkscape,
-//     },
-//     {
-//         name: "java",
-//         icon: java,
-//     },
-//     {
-//         name: "mysql",
-//         icon: mysql,
-//     },
-//     {
-//         name: "photoshop",
-//         icon: photoshop,
-//     },
-//     {
-//         name: "python",
-//         icon: python,
-//     },
-//     {
-//         name: "github",
-//         icon: github,
-//     },
-// ];
 
 import csi from '../assets/experience/csi.png'
 import msc from '../assets/experience/msc.png'
