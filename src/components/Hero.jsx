5e6460d9439b9f8a9775a577e12c2c4fb9e0b49c
Satyam Mishra <satyam.work.only@gmail.com>
2023-09-25 19:32:26 +0530

    added work section project animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {styles} from "../styles";
 import {motion} from "framer-motion";
-import waves from '../assets/waves.png'
+// import waves from '../assets/waves.png'
 // import {FallingBalls} from "./canvas";
 // import CanvasLoader from "./Loader";
 
