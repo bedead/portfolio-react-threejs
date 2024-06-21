a44ca4826319714aef453f2170926e11897dc867
Satyam Mishra <satyammishra9050@gmail.com>
2024-06-21 12:13:14 +0530

    updated resume pdf and cover images

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ghPages } from 'vite-plugin-gh-pages'
+//import { ghPages } from 'vite-plugin-gh-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/portfolio-react-threejs",
   plugins: [
     react(),
-    ghPages(),
+    //ghPages(),
   ],
 });
