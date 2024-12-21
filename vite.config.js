fc143185c710b18a2dd3e2fbbc158a7dc74b5ef7
Satyam Mishra <satyammishra9050@gmail.com>
2024-12-21 20:31:59 +0530

    added vercel.json file | vercel endpoint not working in vercel

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "",
+  base: "/",
   plugins: [
     react(),
   ],
-  server: {
-    host: true
-  }
+  // server: {
+  //   host: true
+  // }
 
 });
