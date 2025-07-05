339a81ad659149761468372d34f4f36e6ad68c28
Satyam Mishra <satyammishra9050@gmail.com>
2025-07-05 14:32:55 +0530

    chore: update packages

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
   plugins: [
-    react(),
+    react()
   ],
   server: {
     host: true
-  }
+  },
 
 });
